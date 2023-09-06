5f5d0a98dae4ec5a92f90f564d60c2e2e504f155
Carlos Sirias <carlos.sirias04@gmail.com>
2023-09-05 19:36:50 -0700

    :sparkles: Services Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 ScrollReveal({ distance: '60px' });
 ScrollReveal().reveal('.__headline', { origin: 'top' });
 ScrollReveal().reveal('.__text', { delay: 500});
+ScrollReveal().reveal('.__about_us', {origin: 'bottom'});
 ScrollReveal().reveal('.__a', { delay: 500});
