68f092fa2b98828d91e8de9c619901f80bf1a2b0
Carlos Sirias <carlos.sirias04@gmail.com>
2023-09-06 22:31:29 -0700

    :sparkles:

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,5 @@ ScrollReveal({ distance: '60px' });
 ScrollReveal().reveal('.__headline', { origin: 'top' });
 ScrollReveal().reveal('.__text', { delay: 500});
 ScrollReveal().reveal('.__about_us', {origin: 'bottom'});
+ScrollReveal().reveal('.__contact_us', {origin: 'bottom'});
 ScrollReveal().reveal('.__a', { delay: 500});
